e9bf3351d82cd3d0c812b63997213ea25001fed8
Amanda Brízido dos Santos <brizidoamanda@gmail.com>
2025-09-27 11:15:19 -0300

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y,z;
+    switch(7){
+        case 1:
+         printf("Entrou no case 1");
+         break;
+        case 2:
+         printf("\nEntrou no case 2");
+         break;
+        case 3:
+         printf("\nEntrou no case 3");
+         break;
+        case 4:
+         printf("\nEntrou no case 4");
+         break;
+        
+        default:
+        printf("Não encontrou o valor");
+    }
+
+    return 0;
+}
